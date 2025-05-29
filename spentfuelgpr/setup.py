3eebf0b2a28d58e8d1a0c1ebdae0067cc68c6f50
Max Schalz <max.schalz@rwth-aachen.de>
2025-05-29 10:57:57 +0200

    Add pyproject.toml to spentfuelgpr and update install procedures

--- spentfuelgpr/setup.py
+++ spentfuelgpr/setup.py
@@ -3,24 +3,4 @@
 
 from setuptools import setup
 
-
-def main():
-    setup(
-        name="spentfuelgpr",
-        version="0.1",
-        description="Python part of the MIsoEnrichment module",
-        author="Nuclear Verification and Disarmament Group, RWTH Aachen University",
-        url="https://github.com/maxschalz/miso_enrichment/",
-        license="BSD-3-Clause",
-        packages=["spentfuelgpr"],
-        classifiers=[
-            "License :: OSI Approved :: BSD-3-Clause License",
-            "Programming Language :: Python :: 3",
-        ],
-        install_requires=["numpy", "scipy"],
-    )
-    return
-
-
-if __name__ == "__main__":
-    main()
+setup()
